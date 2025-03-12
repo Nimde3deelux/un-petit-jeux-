0fc3cf3d1292a2000dab6450dafad042460f6b90
Nimde3deelux <adamsehili28@gmail.com>
2025-03-12 08:38:06 +0100

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const board = Chessboard('board', {
-    draggable: true,
-    position: 'start',
-    onDrop: (source, target, piece) => {
-        console.log(`Pièce déplacée de ${source} à ${target}`);
-    }
-});

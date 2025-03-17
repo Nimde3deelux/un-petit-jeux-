a731489609ebe6500867d6f9b7f2367e836460ba
Nimde3deelux <adamsehili28@gmail.com>
2025-03-17 13:21:41 +0100

    Update game.js

--- game.js
+++ game.js
@@ -1,119 +1,131 @@
-// Game Configuration (phaser setup)
-var config = {
+class MainMenu extends Phaser.Scene {
+    constructor() {
+        super({ key: 'MainMenu' });
+    }
+
+    preload() {
+        this.load.image('background', 'assets/background.png');
+    }
+
+    create() {
+        this.add.image(400, 300, 'background');
+
+        this.add.text(220, 250, 'Appuyez sur "Entrée" pour commencer', {
+            font: '24px Arial',
+            fill: '#ffffff'
+        });
+
+        this.input.keyboard.on('keydown-ENTER', () => {
+            this.scene.start('GameScene');
+        });
+    }
+}
+
+class GameScene extends Phaser.Scene {
+    constructor() {
+        super({ key: 'GameScene' });
+    }
+
+    preload() {
+        this.load.image('player', 'assets/player.png');
+        this.load.image('zombie', 'assets/zombie.png');
+        this.load.image('bullet', 'assets/shot.png');
+        this.load.image('background', 'assets/background.png');
+        this.load.audio('shoot', 'assets/sound_effects/shoot.wav');
+        this.load.audio('hitZombie', 'assets/sound_effects/hit_zombie.wav');
+    }
+
+    create() {
+        // Ajouter le fond
+        this.add.image(400, 300, 'background');
+
+        // Ajouter le joueur
+        this.player = this.physics.add.image(100, 450, 'player').setScale(0.5);
+        this.player.setCollideWorldBounds(true);
+
+        // Ajouter les zombies
+        this.zombies = this.physics.add.group({
+            key: 'zombie',
+            repeat: 5,
+            setXY: { x: 400, y: 0, stepX: 150 }
+        });
+
+        this.zombies.children.iterate((zombie) => {
+            zombie.setBounce(1);
+            zombie.setCollideWorldBounds(true);
+            zombie.setVelocity(Phaser.Math.Between(-200, 200), 20);
+        });
+
+        // Groupes de balles
+        this.bullets = this.physics.add.group({
+            defaultKey: 'bullet',
+            maxSize: 10
+        });
+
+        // Sons
+        this.soundShoot = this.sound.add('shoot');
+        this.soundHit = this.sound.add('hitZombie');
+
+        // Clavier
+        this.cursors = this.input.keyboard.createCursorKeys();
+        this.spaceBar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+
+        // Collisions
+        this.physics.add.collider(this.bullets, this.zombies, this.hitZombie, null, this);
+    }
+
+    update() {
+        // Déplacement du joueur
+        if (this.cursors.left.isDown) {
+            this.player.setVelocityX(-160);
+        } else if (this.cursors.right.isDown) {
+            this.player.setVelocityX(160);
+        } else {
+            this.player.setVelocityX(0);
+        }
+
+        if (this.cursors.up.isDown && this.player.body.touching.down) {
+            this.player.setVelocityY(-330);
+        }
+
+        // Tirer
+        if (Phaser.Input.Keyboard.JustDown(this.spaceBar)) {
+            this.shootBullet();
+        }
+    }
+
+    shootBullet() {
+        let bullet = this.bullets.create(this.player.x + 50, this.player.y, 'bullet');
+        if (bullet) {
+            bullet.setVelocityX(400);
+            this.soundShoot.play();
+        }
+    }
+
+    hitZombie(bullet, zombie) {
+        bullet.destroy();
+        zombie.setTint(0xff0000);
+        zombie.setVelocity(0);
+        this.soundHit.play();
+
+        setTimeout(() => {
+            zombie.clearTint();
+            zombie.setVelocity(Phaser.Math.Between(-200, 200), 20);
+        }, 500);
+    }
+}
+
+// Configuration du jeu
+const config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
-    }
+    physics: {
+        default: 'arcade',
+        arcade: { debug: false }
+    },
+    scene: [MainMenu, GameScene]
 };
 
-var player;
-var zombies;
-var cursors;
-var bullets;
-var soundShoot;
-var soundHit;
-
-var game = new Phaser.Game(config);
-
-// Charger les ressources
-function preload() {
-    this.load.image('player', 'assets/player.png');
-    this.load.image('zombie', 'assets/zombie.png');
-    this.load.image('bullet', 'assets/shot.png');
-    this.load.audio('shoot', 'assets/sound_effects/shoot.wav');
-    this.load.audio('hitZombie', 'assets/sound_effects/hit_zombie.wav');
-}
-
-// Créer les objets et initialiser le jeu
-function create() {
-    // Création du joueur
-    player = this.physics.add.image(100, 450, 'player').setScale(0.5);
-    player.setCollideWorldBounds(true);
-
-    // Création des zombies
-    zombies = this.physics.add.group({
-        key: 'zombie',
-        repeat: 5,
-        setXY: { x: 400, y: 0, stepX: 150 }
-    });
-    zombies.children.iterate(function(zombie) {
-        zombie.setBounce(1);
-        zombie.setCollideWorldBounds(true);
-        zombie.setVelocity(Phaser.Math.Between(-200, 200), 20);
-    });
-
-    // Groupes de balles
-    bullets = this.physics.add.group({
-        defaultKey: 'bullet',
-        maxSize: 10
-    });
-
-    // Sons
-    soundShoot = this.sound.add('shoot');
-    soundHit = this.sound.add('hitZombie');
-
-    // Cursors pour le déplacement
-    cursors = this.input.keyboard.createCursorKeys();
-
-    // Menu principal
-    var text = this.add.text(300, 200, 'Appuyez sur "Entrée" pour commencer', { font: '32px Arial', fill: '#fff' });
-    this.input.keyboard.on('keydown-ENTER', startGame, this);
-}
-
-// Démarrer le jeu
-function startGame() {
-    // Commence le jeu une fois "Entrée" pressée
-    this.scene.start('gameScene');
-}
-
-// Gestion du gameplay
-function update() {
-    // Déplacement du joueur
-    if (cursors.left.isDown) {
-        player.setVelocityX(-160);
-    } else if (cursors.right.isDown) {
-        player.setVelocityX(160);
-    } else {
-        player.setVelocityX(0);
-    }
-
-    if (cursors.up.isDown && player.body.touching.down) {
-        player.setVelocityY(-330);
-    }
-
-    // Tirer
-    if (cursors.space.isDown) {
-        shootBullet();
-    }
-}
-
-// Créer les balles
-function shootBullet() {
-    var bullet = bullets.get(player.x + 50, player.y);
-    if (bullet) {
-        bullet.setActive(true);
-        bullet.setVisible(true);
-        bullet.setVelocityX(400);
-        soundShoot.play();
-    }
-}
-
-// Collision entre les balles et les zombies
-function hitZombie(bullet, zombie) {
-    bullet.setActive(false);
-    bullet.setVisible(false);
-    zombie.setTint(0xff0000);
-    zombie.setVelocity(0);
-    soundHit.play();
-    setTimeout(() => {
-        zombie.clearTint();
-        zombie.setVelocity(Phaser.Math.Between(-200, 200), 20);
-    }, 500);
-}
-
-// Ajouter les collisions
-this.physics.add.collider(bullets, zombies, hitZombie, null, this);
+// Lancer le jeu
+const game = new Phaser.Game(config);
